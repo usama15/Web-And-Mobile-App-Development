5c58872662b51ef0c70e9af46efe744ded1c64fb
Tehseen Jawed <tehseenjawed1@gmail.com>
2020-02-28 11:03:00 +0500

    15 Chapters of JavaScript

---
+++ JaveScript/app.js
@@ -0,0 +1,42 @@
+// alert("Hello World");
+// console.log("Helo World");
+// var a =1;
+// alert(a);
+// x = '15';
+// y= '20';
+// console.log(x+y);
+// var a = +prompt("Enter your votes","Enter Your Values");
+// var b = 20;
+// var c = a + b;
+// alert(c);
+//       POST INCREMENT
+// var a = 2
+// console.log(a++)
+// console.log(a)
+// console.log(a)
+// CONDITION IF
+// var age = +prompt('Enter Your Age')
+// if(age === 51){
+//     alert('You are very young')
+// }
+// else if(age >51){
+//     alert('Isse Acha to Mar Ja')
+// }
+// else{
+//     alert('<b>Khamoooosh</b>')
+// }
+//           Sets of Conditions
+var a = prompt('Enter A')
+var b = prompt('Enter B')
+if (a==='2'){
+    console.log('You are in the condition')
+    if(b === 5){
+        console.log('Your are in the nested condition')
+    }
+    else{
+        console.log('You are in the nested else condition')
+    }
+}
+else{
+ console.log('You are in else condition')   
+}
